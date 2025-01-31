75200a0d2e0f3cc34bb33996074155a04b0fc28b
Zohar <Zshtamberg@campus.technion.ac.il>
2025-01-31 13:44:58 +0200

    init

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "super-tab", -- Use the 'super-tab' preset for Tab navigation
+      },
+    },
+  },
+}
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = function()
+        vim.cmd([[colorscheme slate]])
+      end,
+    },
+  },
+}
